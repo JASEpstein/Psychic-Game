1d3e3a9dc4a599d28c76fc1efb64104a16fc9b1d
JASEpstein <jasepstein@gmail.com>
2019-02-06 22:09:37 -0600

    G vars declared, numGen works, key listener compares numGen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,51 @@
+/*
+on page load, there is a random letter
+
+user must press keys to guess what that letter is
+
+if the key pressed matches the letter, they score a win point
+
+if the key does not match 10 times in a row, then they gain a loss point
+
+*/
+
 var winsCount = 0;
-document.getElementById(winsCount).innerHTML = winsCount;
 var lossesCount = 0;
-document.getElementById(lossesCount).innerHTML = lossesCount;
-var guessesLeft = 10;
-document.getElementById(guessesLeft).innerHTML = guessesLeft;
-var guessesSoFar;
-document.getElementById(guessesSoFar).innerHTML = guessesSoFar;
+var rounds = 0;
+var chars = "abcdefghijklmnopqrstuvwxyz";
+var randomLetter = "";
+
+function numGen(){
+    randomLetter = chars.substr( Math.floor(Math.random() * 26), 1);
+    alert(randomLetter);
+}
 
 document.onkeyup = function(e) {
-    var rletter = (function random_character() {
-        var chars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
-        return chars.substr( Math.floor(Math.random() * 52), 1);
-    })
-    if (rletter === e.key){
-        alert("You guessed it!!");
-        document.getElementById(winsCount).innerHTML = winsCount++;
-    } else {
-        document.getElementById()
+        if (rletter === e.key){
+            document.getElementById("winsCount").innerHTML = winsCount++;
+            alert("You did it!!");
+        } else {
+            document.getElementById("guessesSoFar").innerHTML += ", " + e.key;
+        }
     }
+
+
+
+
+
+// var winsCount = 0;
+// document.getElementById(winsCount).innerHTML = winsCount;
+// var lossesCount = 0;
+// document.getElementById(lossesCount).innerHTML = lossesCount;
+// var guessesLeft = 10;
+// document.getElementById(guessesLeft).innerHTML = guessesLeft;
+// var guessesSoFar;
+
     
-}
+    
+
+// console.log(rletter);
+
+
+
+
